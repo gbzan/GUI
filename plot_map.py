48daa2fd45bd31fbb0445bc19fdbd57d1317a526
Xiaolin Yang <yangxiaolin.sbu@gmail.com>
2021-09-09 18:20:00 -0700

    Added a qslider widget to get value of step point and update the plot simultaneously.

--- plot_map.py
+++ plot_map.py
@@ -1,10 +1,11 @@
 from PIL.Image import EXTENT
 import numpy as np
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QPushButton
+from PyQt5.QtWidgets import QHBoxLayout, QLabel, QSlider, QWidget, QVBoxLayout, QPushButton
 import matplotlib.pyplot as plt
 from data import Data
 from matplotlib.backends.backend_template import FigureCanvas
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas, NavigationToolbar2QT as NavigationToolbar
+from PyQt5.QtCore import Qt
 
 
 class PlotMap(QWidget):
@@ -14,6 +15,7 @@ class PlotMap(QWidget):
         x_offset, y_offset = data.current_roi[0], data.current_roi[1]
         x_len = len(data.current_datacube[0, 0, :])  
         y_len = len(data.current_datacube[0, :, 0])  
+        z_len = len(data.current_datacube[:, 0, 0])  
         # Create the plot.
         figure, axes = plt.subplots(1, 1)
         axes.imshow(array[0, :, :], interpolation='nearest', extent=[x_offset, x_offset+x_len-1, y_offset+y_len-1, y_offset])
@@ -23,6 +25,30 @@ class PlotMap(QWidget):
         figure.set_facecolor("#C9e8f1")
         plot = FigureCanvas(figure)
 
+        # Create Slider Widget
+        slider_widget = QWidget()
+        slider_widget.setFixedHeight(40)
+        label_widget = QLabel('0')
+        
+        step_slider = QSlider(Qt.Horizontal)
+        step_slider.setRange(0, z_len-1)
+        step_slider.setFocusPolicy(Qt.StrongFocus)
+        step_slider.setTickPosition(QSlider.TicksAbove)
+        step_slider.setTickInterval(1)
+        step_slider.setSingleStep(1)
+
+
+        def slider_update(val):
+            label_widget.setText(str(val))
+            axes.imshow(array[val, :, :], interpolation='nearest', extent=[x_offset, x_offset+x_len-1, y_offset+y_len-1, y_offset])
+            plot.draw()
+        step_slider.valueChanged.connect(slider_update)
+
+        slider_layout = QHBoxLayout()
+        slider_layout.addWidget(step_slider)
+        slider_layout.addWidget(label_widget)
+        slider_widget.setLayout(slider_layout)
+
         # Create the toolbar.
         toolbar = NavigationToolbar(plot, self)
 
@@ -30,4 +56,9 @@ class PlotMap(QWidget):
         layout = QVBoxLayout()
         layout.addWidget(plot)
         layout.addWidget(toolbar)
+        layout.addWidget(slider_widget)
         self.setLayout(layout)
+
+        
+            
+    
